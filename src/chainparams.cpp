15785c6ed1386e5e7d8b5fb63dc5d74760481f65
Didy Septiyono <didy@didy.project>
2018-08-08 10:57:26 +0700

    make it better

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -55,16 +55,16 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
     (0, uint256("00000a8ad8ca5050f8d98609f0d75d617ca6444a285b8aa5d9359162259dfcaf"))
-    // (1, uint256("00013088d24a802b9eb67fb8199090072f656d37b6e714e48382dd8dfb3945c7"))
-    // (12, uint256("0001510430304bdfee306e8d0d7c5a7c714984f97a0f71cc00824ba66306d4e9"))
-    // (33, uint256("00001f9bcc9cabec14a4762bb15eeb45df226f4c252c764d2aec2f5a105d7e5a"))
-    // (55, uint256("0000854af0ce3dff9b02d4214c8aa7bf2794672a97eb8080f9882c1fa87122f7"))
-    // (77, uint256("0001f3f4fde38ff767475067bdbc80a1acfec2f0b179ace6a2aceec9d2bcf601"))
-    // (99, uint256("0000bed5ec0151bee2525d517073385cd6afdb756ddc401bd82cbd6eae26eaa8"))
-    // (155, uint256("000001551ee304d7765a76e20a5cbe9785c916730e7f34e0ce3cde0ba9f0e7ba"))
-    // (222, uint256("999c7c57001920864eafe6ed7070d3a4ada35dc252fc2450fd0756c57c80e685"))
-    // (456, uint256("d3816e97cd8ca4f1730e8f16ab6faed73d31da2aec04479213692fb5c8301044"))
-    ;
+    (99, uint256("000000329ce45df2cdd30e9f3de6ba3c5a091d8e4c803c0145f7c8880416c2da"))
+    (149, uint256("0000030e805aa0530be3c026a7e0405589c4857d0eb20d1f6272c98c4ee98c86"))
+    (199, uint256("0000017b22c2419be795dc3ef8159f0e2e4dac0d8621f03b95c7112a4cebe5de"))
+    (299, uint256("c0858437f42bb4cff1a0bae661c746329575f40db08fdb722ce3b0b94f086cf0"))
+    (333, uint256("89db01dce161b037d5379ee92ebf406f6d63a7a493576d21201ff1c03113f845"))
+    (399, uint256("6ba29a39d1f1427e859db9e58d36ff45f07072234c5d4efc39bf03b24ee9c04e"))
+    (499, uint256("44d917844a8e6eb00f42284ffc6bbafa53b4d1d7357dafee49266729477af894"))
+    (522, uint256("9ed0fe9f9ed4ba8366fd070309c4da6ab3b1190ef441dbed125cabdba3389db7"))
+    (544, uint256("d89b1504b121a01a853efbe04365298c126ecbdfe3e03e3d5f9b46e7337446b0"))
+    (555, uint256("f0f917e732f024ed20407f330808f80e3cf3b440608839f7db1c138f5b1f49fb"));
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1530489328, // * UNIX timestamp of last checkpoint block
